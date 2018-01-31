67187bc25e0195829f5248655cd657daf82381a3
Steven K Word <stevenword@gmail.com>
2018-01-31 13:24:14 -0600

    Seed with WP-CLI scaffold

---
+++ gutenberg-compatibility-checker.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Gutenberg Compatibility Checker
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     gutenberg-compatibility-checker
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gutenberg_Compatibility_Checker
+ */

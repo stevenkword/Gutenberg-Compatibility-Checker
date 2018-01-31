8724548be86d5b23c95a1e7e1a5eac1904e5af6a
Steven K Word <stevenword@gmail.com>
2018-01-31 13:28:53 -0600

    Readme Updates

--- gutenberg-compatibility-checker.php
+++ gutenberg-compatibility-checker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Gutenberg Compatibility Checker
  * Plugin URI:      https://github.com/stevenkword/Gutenberg-Compatibility-Checker
- * Description:     Check your WordPress Plugins for compatibility with Gutenberg
+ * Description:     Check your WordPress Plugins for compatibility with Gutenberg.
  * Author:          Steven Word
  * Author URI:      https://stevenword.com
  * Text Domain:     gutenberg-compatibility-checker
--- readme.txt
+++ readme.txt
@@ -7,7 +7,7 @@ Stable tag: 1.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-Check your WordPress Plugins for compatibility with Gutenberg
+Check your WordPress Plugins for compatibility with Gutenberg.
 
 == Description ==
 
